9254a73386d60ab36ecb65fd622d8c6a64408f75
IT21108990 <IT21108990@my.sliit.lk>
2023-07-13 22:18:43 +0530

    create page

--- nextjs/app/api/auth/[...nextauth]/route.js
+++ nextjs/app/api/auth/[...nextauth]/route.js
@@ -4,45 +4,43 @@ import { connectToDB } from "@utils/database";
 import User from "@models/User";
 
 const handler = NextAuth({
-    providers: [
-        GoogleProvider({
-            clientId: process.env.GOOGLE_ID,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        }),
-    ],
-    callbacks: {
-        async session({ session }) {
-            const sessionUser = await User.findOne({ email: session.user.email });
-            
-            session.user.id = sessionUser._id.toString();
-            return session;
-        
-        },
-    
-        async signIn({ profile }) {
-            try {
-                // serverless -> lambda -> dynamodb
-                await connectToDB();
-    
-                // check if user already exists in db
-                const userExists = await User.findOne({ email: profile.email });
-                
-                //if not, create user
-                if(!userExists) {
-                    await User.create({
-                        email: profile.email,
-                        username: profile.name.replace(" ", "").tolowercase(),
-                        image: profile.picture
-                    })
-                }
-    
-                return true;
-            } catch (error) {
-                console.log(error);
-                return true;
-            }
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+  ],
+  callbacks: {
+    async session({ session }) {
+      const sessionUser = await User.findOne({ email: session.user.email });
+
+      session.user.id = sessionUser._id.toString();
+      return session;
+    },
+
+    async signIn({ profile }) {
+      try {
+        // serverless -> lambda -> dynamodb
+        await connectToDB();
+
+        // check if user already exists in db
+        const userExists = await User.findOne({ email: profile.email });
+
+        //if not, create user
+        if (!userExists) {
+          await User.create({
+            email: profile.email,
+            username: profile.name.replace(" ", "").tolowercase(),
+            image: profile.picture,
+          });
         }
-    }
-    
+
+        return true;
+      } catch (error) {
+        console.log(error);
+        return true;
+      }
+    },
+  },
 });
-export { handler as GET, handler as POST }
+export { handler as GET, handler as POST };
